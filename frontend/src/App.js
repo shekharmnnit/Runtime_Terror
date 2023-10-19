c14eecf1caa905a913e4f89ace27685a2161163d
Laxman M <rafadeinol@gmail.com>
2023-10-19 19:50:05 -0400

    added login/registration

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,17 @@
 // import logo from './logo.svg';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import './App.css';
-import Login from './components/App'
+import Login from './components/Login.tsx'
 function App() {
   return (
-    <Login/>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />}>
+        </Route>
+      </Routes>
+    </BrowserRouter>
+    // <Login/>
   );
 }
 
