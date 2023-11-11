513671a0b87af391073ae6fbf8e027401c9123e2
Lavisha1842 <lavishagoyal2000@gmail.com>
2023-11-11 13:04:19 -0500

    Show tags on profile, edit post detail

--- frontend/src/components/UserProfileDetail.tsx
+++ frontend/src/components/UserProfileDetail.tsx
@@ -9,6 +9,9 @@ function UserProfileDetail({ obj }) {
     let firstName = String(localStorage.getItem('local_first_name'));
     let lastName = String(localStorage.getItem('local_last_name'));
     let email = String(localStorage.getItem('local_reg_email'));
+    let storedSkillsString = String(localStorage.getItem("local_user_skills"));
+    let storedSkills = JSON.parse(storedSkillsString) || [];
+    
     let path = window.location.pathname;
     console.log(path);
     const hasDigits = /\d/.test(path);
@@ -51,6 +54,9 @@ function UserProfileDetail({ obj }) {
                 </div>
                 <div className="user-profile-details">
                     {profileObj.email}
+                </div>
+                <div className="user-profile-details">
+                    Skills: {storedSkills.filter(skill => skill.trim() !== '').join(', ')}
                 </div>
                 <button className="edit-button" onClick={handleEditClick}>
                     Edit
